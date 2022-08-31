7cc0ac562cfe024b9fb7d24d7f648aec660cb03d
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-09-01 00:00:33 +0600

    data fetched

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// load data
+const loadCocktails = async () => {
+  const res = await fetch(
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+  );
+  const data = await res.json();
+  displayAllCocktails(data.drinks);
+};
+
+const displayAllCocktails = (cocktails) => {
+  const container = document.getElementById("cocktail-container");
+
+  cocktails.forEach((cocktail) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+
+    div.innerHTML = `
+    <div class="card">
+    <img src="${cocktail.strDrinkThumb}" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">${cocktail.strDrink}</h5>
+      <p class="card-text">
+        ${cocktail.strInstructions}
+      </p>
+    </div>
+  </div>
+    `;
+
+    container.appendChild(div);
+  });
+};
+
+loadCocktails();
